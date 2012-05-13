c2a76138435da7a0c2d07591018a1957ccbca5bb
Aaron Iles <aaron.iles@gmail.com>
2012-05-13 21:08:08 +1000

    Import modules from __all__ for tests.
    Import all modules from tls.api to ensure modules are present in
    __all__ variable.

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,5 +1,6 @@
 import unittest
 
+from tls.api import *
 import tls.api
 
 
@@ -13,32 +14,31 @@ class TestTlsApi(unittest.TestCase):
 
     def test_bio(self):
         self.assertIn('bio', tls.api.__all__)
-        self.assertGreater(len(tls.api.bio.__all__), 0)
+        self.assertGreater(len(bio.__all__), 0)
 
     def test_constant(self):
         self.assertIn('constant', tls.api.__all__)
-        self.assertGreater(len(tls.api.constant.__all__), 0)
+        self.assertGreater(len(constant.__all__), 0)
 
     def test_digest(self):
         self.assertIn('digest', tls.api.__all__)
-        self.assertGreater(len(tls.api.digest.__all__), 0)
+        self.assertGreater(len(digest.__all__), 0)
 
     def test_error(self):
         self.assertIn('error', tls.api.__all__)
-        self.assertGreater(len(tls.api.error.__all__), 0)
+        self.assertGreater(len(error.__all__), 0)
 
     def test_exceptions(self):
         self.assertIn('exceptions', tls.api.__all__)
-        self.assertGreater(len(tls.api.exceptions.__all__), 0)
+        self.assertGreater(len(exceptions.__all__), 0)
 
     def test_nid(self):
         self.assertIn('nid', tls.api.__all__)
-        self.assertGreater(len(tls.api.nid.__all__), 0)
+        self.assertGreater(len(nid.__all__), 0)
 
     def test_objects(self):
         self.assertIn('objects', tls.api.__all__)
-        self.assertGreater(len(tls.api.objects.__all__), 0)
 
     def test_rand(self):
         self.assertIn('rand', tls.api.__all__)
-        self.assertGreater(len(tls.api.rand.__all__), 0)
+        self.assertGreater(len(rand.__all__), 0)
