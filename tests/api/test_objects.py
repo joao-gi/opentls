f1998ee8d3cd6581b8e891fefdb4658087e926a9
Aaron Iles <aaron.iles@gmail.com>
2012-07-12 22:44:36 +1000

    Port OBJ API unittests to new cffi API.
    Ensures OBJ API is used to convert object ids to names and back again.

--- tests/api/test_objects.py
+++ tests/api/test_objects.py
@@ -1,68 +1,61 @@
 """Test objects API"""
 import unittest2 as unittest
 
-from tls.api import objects, nid
-from tls.api.constant import OBJ_NAME_TYPE_MD_METH
+from tls.c import api
 
 
 class TestNidTo(unittest.TestCase):
 
-    @unittest.skip('needs to be ported to cffi')
     def test_nid2ln(self):
-        name = objects.OBJ_nid2ln(nid.MD5)
-        self.assertEqual(b'md5', name)
+        name = api.OBJ_nid2ln(api.NID_md5)
+        self.assertEqual(b'md5', bytes(name))
 
-    @unittest.skip('needs to be ported to cffi')
     def test_nid2ln_error(self):
-        self.assertRaises(objects.ASNError, objects.OBJ_nid2ln, -1)
+        name = api.OBJ_nid2ln(-1)
+        self.assertEqual(api.NULL, name)
 
-    @unittest.skip('needs to be ported to cffi')
     def test_nid2sn(self):
-        name = objects.OBJ_nid2sn(nid.MD5)
-        self.assertEqual(b'MD5', name)
+        name = api.OBJ_nid2sn(api.NID_md5)
+        self.assertEqual(b'MD5', bytes(name))
 
-    @unittest.skip('needs to be ported to cffi')
     def test_nid2sn_error(self):
-        self.assertRaises(objects.ASNError, objects.OBJ_nid2sn, -1)
+        name = api.OBJ_nid2sn(-1)
+        self.assertEqual(api.NULL, name)
 
 
 class TestNameTo(unittest.TestCase):
 
-    @unittest.skip('needs to be ported to cffi')
     def test_ln2nid(self):
-        num = objects.OBJ_ln2nid(b'md5')
-        self.assertEqual(nid.MD5, num)
+        num = api.OBJ_ln2nid(b'md5')
+        self.assertEqual(api.NID_md5, num)
 
-    @unittest.skip('needs to be ported to cffi')
     def test_ln2nid_error(self):
-        self.assertRaises(objects.ASNError, objects.OBJ_ln2nid, b'_undef_')
+        num = api.OBJ_ln2nid(b'_undef_')
+        self.assertEqual(api.NID_undef, num)
 
-    @unittest.skip('needs to be ported to cffi')
     def test_sn2nid(self):
-        num = objects.OBJ_sn2nid(b'MD5')
-        self.assertEqual(nid.MD5, num)
+        num = api.OBJ_sn2nid(b'MD5')
+        self.assertEqual(api.NID_md5, num)
 
-    @unittest.skip('needs to be ported to cffi')
     def test_sn2nid_error(self):
-        self.assertRaises(objects.ASNError, objects.OBJ_sn2nid, b'_undef_')
+        num = api.OBJ_sn2nid(b'_undef_')
+        self.assertEqual(api.NID_undef, num)
 
 
 class TestDoAll(unittest.TestCase):
 
-    @unittest.skip('needs to be ported to cffi')
     def test_do_all(self):
         names = set()
-        def add_name(a, b):
-            names.add(a.contents.name)
-        callback = objects.c_do_all_callback(add_name)
-        objects.OBJ_NAME_do_all(OBJ_NAME_TYPE_MD_METH, callback, None)
+        def add_name(obj, _):
+            names.add(obj.name)
+        callback = api.callback('void(*)(const OBJ_NAME*, void *arg)', add_name)
+        api.OBJ_NAME_do_all(api.OBJ_NAME_TYPE_MD_METH, callback, api.NULL)
         self.assertGreater(len(names), 0)
 
-    @unittest.skip('needs to be ported to cffi')
     def test_do_all_sorted(self):
         names = set()
-        def add_name(a, b):
-            names.add(a.contents.name)
-        callback = objects.c_do_all_callback(add_name)
-        objects.OBJ_NAME_do_all_sorted(OBJ_NAME_TYPE_MD_METH, callback, None)
+        def add_name(obj, _):
+            names.add(obj.name)
+        callback = api.callback('void(*)(const OBJ_NAME*, void *arg)', add_name)
+        api.OBJ_NAME_do_all_sorted(api.OBJ_NAME_TYPE_MD_METH, callback, api.NULL)
         self.assertGreater(len(names), 0)
