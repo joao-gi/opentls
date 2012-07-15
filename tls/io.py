9e470adaadff2f7fde285053af944a9e0f5b53c2
Aaron Iles <aaron.iles@gmail.com>
2012-07-15 23:53:01 +1000

    Python API for OpenSSL's BIO API.
    Enables file like Python objects to be wrapped and used as OpenSSL BIO
    objects.

---
+++ tls/io.py
@@ -0,0 +1,130 @@
+"""IO stream handling using OpenSSL's buffered IO API.
+
+wrap_io(fileobj) - returns an OpenSSL BIO object for the Python file like
+                   object. The caller is required to call api.BIO_free on the
+                   returned object before being garbage collected.
+
+For example, wrapping a StringIO object:
+
+    >>> from tls import io
+    >>> from StringIO import StringIO
+    >>> data = StringIO('Now for something completely different')
+    >>> bio = io.wrap_io(data)
+    >>> buf = api.new('char[]', 3)
+    >>> api.BIO_read(bio, buf, len(buf))
+    3
+    >>> print buf
+    Now
+    >>> api.BIO_free(bio)
+
+"""
+from __future__ import absolute_import
+
+import weakref
+
+from tls.c import api
+
+
+class BIOSourceSink(object):
+    """Presents an OpenSSL BIO method for a file like object.
+
+    The new BIO method is available as the method attribute on class instances.
+    The original object is the fileobj attribute. To create automatically have
+    a BIO object created with the associated method retained until the BIO
+    object is garbage collected, use the wrap_io class method.
+    """
+
+    @classmethod
+    def wrap_io(cls, fileobj):
+        """Create a new BIO object for a file like Python object.
+
+        Returns the cffi.Cdata instance for BIO pointer. The associated method
+        object will be retained until the cffi.Cdata is deleted. The caller is
+        required to call api.BIO_free on the pointer to release memory
+        allocated by OpenSSL.
+        """
+        wrapper = cls(fileobj)
+        bio = api.new('BIO')
+        api.BIO_set(bio, wrapper.method)
+        api.relate(bio, wrapper, 'method')
+        return bio
+
+    def __init__(self, fileobj):
+        method = api.new('BIO_METHOD', coown=True)
+        method.type = api.BIO_TYPE_SOURCE_SINK | 0xFF
+        method.name = api.new('char[]', repr(fileobj).encode())
+        method.bwrite = api.callback('int (*)(BIO*, const char*, int)', self.write)
+        method.bread = api.callback('int (*)(BIO*, char*, int)', self.read)
+        method.bputs = api.callback('int (*)(BIO*, const char*)', self.puts)
+        method.bgets = api.callback('int (*)(BIO*, char*, int)', self.gets)
+        method.ctrl = api.callback('long (*)(BIO*, int, long, void*)', self.ctrl)
+        method.create = api.callback('int (*)(BIO*)', self.create)
+        method.destroy = api.NULL
+        method.callback_ctrl = api.NULL
+        self.method = method._unwrap()
+        self.fileobj = fileobj
+
+    def write(self, bio, data, length):
+        try:
+            self.fileobj.write(api.buffer(data, length))
+            return length;
+        except:
+            return -2
+
+    def read(self, bio, data, length):
+        try:
+            buff = api.buffer(data, length)
+            if hasattr(self.fileobj, 'readinto'):
+                count = self.fileobj.readinto(buff)
+                count = count if count is not None else 0
+            else:
+                tmp = self.fileobj.read(length)
+                count = len(tmp)
+                buff[:count] = tmp
+            return count
+        except:
+            return -2
+
+    def puts(self, bio, data):
+        return -2
+
+    def gets(self, bio, data, length):
+        return -2
+
+    def ctrl(self, bio, cmd, num, obj):
+        try:
+            if cmd == api.BIO_CTRL_FLUSH:
+                self.fileobj.flush()
+                return 1
+            elif cmd == api.BIO_CTRL_RESET:
+                self.fileobj.seek(0)
+                return 0
+            elif cmd == api.BIO_CTRL_SET:
+                return 1
+            elif cmd == api.BIO_CTRL_SET_CLOSE:
+                return 1
+            elif cmd == api.BIO_CTRL_DUP:
+                return 1
+            elif cmd == api.BIO_CTRL_GET_CLOSE:
+                return api.BIO_NOCLOSE
+            elif cmd == api.BIO_CTRL_INFO:
+                return 0
+            elif cmd == api.BIO_CTRL_GET:
+                return 0
+            elif cmd == api.BIO_CTRL_PENDING:
+                return 0
+            elif cmd == api.BIO_CTRL_WPENDING:
+                return 0
+            elif cmd == api.BIO_CTRL_EOF:
+                return -1
+        except:
+            return -1;
+
+    def create(self, bio):
+        bio.init = 1
+        bio.num = 0
+        bio.ptr = api.NULL
+        return 1
+
+
+wrap_io = BIOSourceSink.wrap_io
