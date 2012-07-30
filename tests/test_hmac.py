443fd9063d142b4b709e0fd4032b95a548272a60
Aaron Iles <aaron.iles@gmail.com>
2012-07-30 22:28:44 +1000

    Reimplement Python's hmac module using OpenSSL API
    Largely compatible interface with the hmac module from the Python
    standard library. Deviations in the API are due to limitations in
    OpenSSL's HMAC functions.

---
+++ tests/test_hmac.py
@@ -0,0 +1,69 @@
+"""Test Python hmac API implementation using OpenSSL"""
+import unittest2 as unittest
+
+from .c.test_hmac import Vector001, Vector002, Vector003
+
+from tls.hmac import new
+
+
+class HMACTests(object):
+
+    def test_quick(self):
+        hmac = new(self.key, self.data, self.md)
+        self.assertEqual(self.digest, hmac.digest())
+
+    def test_long(self):
+        hmac = new(self.key, None, self.md)
+        hmac.update(self.data)
+        self.assertEqual(self.digest, hmac.digest())
+
+    def test_multiple_updates(self):
+        hmac = new(self.key, None, self.md)
+        for ch in self.data:
+            hmac.update(ch)
+        self.assertEqual(self.digest, hmac.digest())
+
+    def test_hexdigest(self):
+        hmac = new(self.key, self.data, self.md)
+        hexdigest = hmac.hexdigest()
+        received = []
+        for pos in range(0, len(hexdigest), 2):
+            received.append(int(hexdigest[pos:pos+2], 16))
+        expected = [ord(b) for b in self.digest]
+        self.assertEqual(expected, received)
+
+
+class TestMd5001(HMACTests, Vector001, unittest.TestCase):
+
+    md = None
+    digest = '\x92\x94\x72\x7a\x36\x38\xbb\x1c\x13\xf4\x8e\xf8\x15\x8b\xfc\x9d'
+
+
+class TestMd5002(HMACTests, Vector002, unittest.TestCase):
+
+    md = None
+    digest = '\x75\x0c\x78\x3e\x6a\xb0\xb5\x03\xea\xa8\x6e\x31\x0a\x5d\xb7\x38'
+
+
+class TestMd5003(HMACTests, Vector003, unittest.TestCase):
+
+    md = None
+    digest = '\x56\xbe\x34\x52\x1d\x14\x4c\x88\xdb\xb8\xc7\x33\xf0\xe8\xb3\xf6'
+
+
+class TestSha1001(HMACTests, Vector001, unittest.TestCase):
+
+    md = 'sha1'
+    digest = 'g[\x0b:\x1bM\xdfN\x12Hr\xdal/c+\xfe\xd9W\xe9'
+
+
+class TestSha1002(HMACTests, Vector002, unittest.TestCase):
+
+    md = 'sha1'
+    digest = '\xef\xfc\xdfj\xe5\xeb/\xa2\xd2t\x16\xd5\xf1\x84\xdf\x9c%\x9a|y'
+
+
+class TestSha1003(HMACTests, Vector003, unittest.TestCase):
+
+    md = 'sha1'
+    digest = '\xd70YM\x16~5\xd5\x95o\xd8\x00=\r\xb3\xd3\xf4m\xc7\xbb'
---
+++ tls/hmac.py
@@ -0,0 +1,116 @@
+"""HMAC (Keyed-Hashing for Message Authentication) Python module.
+
+Implements the HMAC algorithm as described by RFC 2104.
+"""
+from tls.c import api
+
+
+class HMAC(object):
+    """RFC 2104 HMAC class.  Also complies with RFC 4231.
+
+    This supports the API for Cryptographic Hash Functions (PEP 247) with the
+    following exceptions:
+
+     - There is now copy() method.
+     - After calling digest() or hexdigest() the HMAC can no longer be updated
+
+    These exceptions are limitations of OpenSSL's HMAC functions.
+    """
+
+    def __init__(self, key, msg=None, digestmod=None):
+        """Create a new HMAC object.
+
+        key:       key for the keyed hash object.
+        msg:       Initial input for the hash, if provided.
+        digestmod: A message digest name. *OR*
+                   A module supporting PEP 247.  *OR*
+                   A hashlib constructor returning a new hash object.
+
+        If module or hashlib constuctor is passed as digestmod the '__name__'
+        and 'args' attributes are searched to find a message digest name. If
+        not provied the digestmod defaults to 'md5'.
+
+        Note: key and msg must be a bytes objects.
+        """
+        if digestmod is None:
+            self._md = api.EVP_md5()
+        else:
+            self._set_md(digestmod)
+        self._ctx = api.new('HMAC_CTX*')
+        self._key = api.new('char[]', key)
+        self._digest = None
+        api.HMAC_Init_ex(self._ctx,
+                api.cast('void*', self._key), len(key), self._md, api.NULL)
+        if msg is not None:
+            self.update(msg)
+
+    def __del__(self):
+        if self._ctx is not None:
+            api.HMAC_CTX_cleanup(self._ctx)
+            self._ctx = None
+
+    def _set_md(self, digestmod):
+        if isinstance(digestmod, bytes):
+            self._md = api.EVP_get_digestbyname(digestmod)
+        if self._md == api.NULL:
+            name = getattr(digestmod, '__name__', '')
+            self._md = api.EVP_get_digestbyname(name)
+        if self._md == api.NULL:
+            for name in getattr(digestmod, 'args', []):
+                self._md = api.EVP_get_digestbyname(name)
+                if self._md != api.NULL:
+                    break
+        if self._md == api.NULL:
+            msg = 'Unknown message digest {0}'.format(repr(digestmod))
+            raise ValueError(msg)
+
+    @property
+    def digest_size(self):
+        return api.EVP_MD_size(self._md)
+
+    def update(self, msg):
+        """Update this hashing object with the string msg.
+        """
+        if self._ctx is None:
+            raise ValueError('HMAC already closed')
+        data = api.new('char[]', msg)
+        api.HMAC_Update(self._ctx, api.cast('void*', data), len(msg))
+
+    def digest(self):
+        """Return the hash value of this hashing object.
+
+        This returns a string containing 8-bit data. After calling digest()
+        it's no longer possible to call update(). The digest value can continue
+        to be retrieved.
+        """
+        if self._digest is not None:
+            return self._digest
+        if self._ctx is None:
+            raise ValueError('HMAC already closed')
+        buff = api.new('unsigned char[]', api.EVP_MAX_MD_SIZE)
+        size = api.new('unsigned int*')
+        api.HMAC_Final(self._ctx, buff, size)
+        self._digest = bytes(api.buffer(buff, size[0]))
+        self.__del__()
+        return self._digest
+
+    def hexdigest(self):
+        """Like digest(), but returns a string of hexadecimal digits instead.
+        """
+        if self._ctx is None:
+            raise ValueError('HMAC already closed')
+        return ''.join('{0:02x}'.format(ord(b)) for b in self.digest())
+
+
+def new(key, msg=None, digestmod=None):
+    """Create a new hashing object and return it.
+
+    key: The starting key for the hash.
+    msg: if available, will immediately be hashed into the object's starting
+    state.
+
+    You can continue to feed strings into the object using its update() method.
+    When complete the hash value can be retrieved by calling the digest() or
+    hexdigest() method.
+    """
+    return HMAC(key, msg, digestmod)
