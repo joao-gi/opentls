bd89864c78634f92a8db80105e8cdcf0c59f0005
Aaron Iles <aaron.iles@gmail.com>
2012-08-05 22:38:47 +1000

    Add Cipher object to cipherlib
    The Cipher object represents a cipher algorithm, with a number of
    properties for the underlying algorithm such as block size and key
    length.

--- tests/test_cipherlib.py
+++ tests/test_cipherlib.py
@@ -1,7 +1,9 @@
 """Test Python cipherlib API module"""
 import unittest2 as unittest
+import mock
 
 from tls import cipherlib
+from tls.c import api
 
 
 class TestAlgorithms(unittest.TestCase):
@@ -12,3 +14,104 @@ class TestAlgorithms(unittest.TestCase):
     def test_available(self):
         self.assertGreater(len(cipherlib.algorithms_available), 0)
         self.assertIn(b'AES-128-CBC', cipherlib.algorithms_available)
+
+
+class TestAesEncryptObject(unittest.TestCase):
+
+    ALGORITHM = 'AES-128-CBC'
+    ENCRYPT = True
+    IVECTOR = '\x00' * 16
+    KEY = '\x00' * 16
+
+    def setUp(self):
+        self.cipher = cipherlib.Cipher(self.ENCRYPT, self.ALGORITHM)
+
+    def tearDown(self):
+        if hasattr(self, 'cipher'):
+            del self.cipher
+
+    def test_block_size(self):
+        self.assertEqual(16, self.cipher.block_size)
+
+    def test_ivector_len(self):
+        self.assertEqual(16, self.cipher.ivector_len)
+
+    def test_key_len(self):
+        self.assertEqual(16, self.cipher.key_len)
+
+    def test_key_len_set(self):
+        def change_key_len(length):
+            self.cipher.key_len = length
+        self.assertRaises(ValueError, change_key_len, 8)
+
+    def test_decrypting(self):
+        self.assertFalse(self.cipher.decrypting)
+
+    def test_encrypting(self):
+        self.assertTrue(self.cipher.encrypting)
+
+    def test_mode(self):
+        self.assertEqual(cipherlib.EVP_CIPH_CBC_MODE, self.cipher.mode)
+
+    def test_name(self):
+        self.assertEqual('AES-128-CBC', self.cipher.name)
+
+    def test_invalid_name(self):
+        self.assertRaises(ValueError, cipherlib.Cipher, self.ENCRYPT, 'UNDEF')
+
+    def test_weakref(self):
+        EVP_CIPHER_CTX_cleanup = api.EVP_CIPHER_CTX_cleanup
+        with mock.patch('tls.c.api.EVP_CIPHER_CTX_cleanup') as cleanup_mock:
+            cleanup_mock.side_effect = EVP_CIPHER_CTX_cleanup
+            del self.cipher
+            self.assertEqual(cleanup_mock.call_count, 1)
+
+
+class TestRc4DecryptObject(unittest.TestCase):
+
+    ALGORITHM = 'RC4'
+    ENCRYPT = False
+    IVECTOR = ''
+    KEY = '\x00' * 16
+
+    def setUp(self):
+        self.cipher = cipherlib.Cipher(self.ENCRYPT, self.ALGORITHM)
+
+    def tearDown(self):
+        if hasattr(self, 'cipher'):
+            del self.cipher
+
+    def test_block_size(self):
+        self.assertEqual(1, self.cipher.block_size)
+
+    def test_ivector_len(self):
+        self.assertEqual(0, self.cipher.ivector_len)
+
+    def test_key_len(self):
+        self.assertEqual(16, self.cipher.key_len)
+
+    def test_key_len_set(self):
+        self.cipher_key_len = 8
+        # self.assertEqual(8, self.cipher.key_len)
+
+    def test_decrypting(self):
+        self.assertTrue(self.cipher.decrypting)
+
+    def test_encrypting(self):
+        self.assertFalse(self.cipher.encrypting)
+
+    def test_mode(self):
+        self.assertEqual(cipherlib.EVP_CIPH_STREAM_CIPHER, self.cipher.mode)
+
+    def test_name(self):
+        self.assertEqual('RC4', self.cipher.name)
+
+    def test_invalid_name(self):
+        self.assertRaises(ValueError, cipherlib.Cipher, self.ENCRYPT, 'UNDEF')
+
+    def test_weakref(self):
+        EVP_CIPHER_CTX_cleanup = api.EVP_CIPHER_CTX_cleanup
+        with mock.patch('tls.c.api.EVP_CIPHER_CTX_cleanup') as cleanup_mock:
+            cleanup_mock.side_effect = EVP_CIPHER_CTX_cleanup
+            del self.cipher
+            self.assertEqual(cleanup_mock.call_count, 1)
--- tls/cipherlib.py
+++ tls/cipherlib.py
@@ -7,12 +7,96 @@ algorithms that are guaranteed to be available on all platforms.
 from collections import namedtuple
 import weakref
 
+from tls import err
 from tls.c import api
 from tls.util import all_obj_type_names as __available_algorithms
 
-__all__ = ['algorithms_available', 'algorithms_guaranteed']
+__all__ = [
+    'algorithms_available',
+    'algorithms_guaranteed',
+    'Cipher',
+    'EVP_CIPH_ECB_MODE',
+    'EVP_CIPH_CBC_MODE',
+    'EVP_CIPH_CFB_MODE',
+    'EVP_CIPH_OFB_MODE',
+    'EVP_CIPH_STREAM_CIPHER',
+]
 
 
 # there are no guarantees with openssl
 algorithms_guaranteed = set()
 algorithms_available = __available_algorithms(api.OBJ_NAME_TYPE_CIPHER_METH)
+
+
+# cipher modes
+EVP_CIPH_ECB_MODE = api.EVP_CIPH_ECB_MODE
+EVP_CIPH_CBC_MODE = api.EVP_CIPH_CBC_MODE
+EVP_CIPH_CFB_MODE = api.EVP_CIPH_CFB_MODE
+EVP_CIPH_OFB_MODE = api.EVP_CIPH_OFB_MODE
+EVP_CIPH_STREAM_CIPHER = api.EVP_CIPH_STREAM_CIPHER
+
+
+class Cipher(object):
+    """A cipher object is used to encrypt plaintext or decrypt ciphertext.
+    """
+
+    def __init__(self, encrypt=True, algorithm='AES-128-CBC'):
+        self._ctx = api.NULL
+        self._cipher = api.EVP_get_cipherbyname(algorithm)
+        self._encrypting = bool(encrypt)
+        if self._cipher == api.NULL:
+            msg = "Unknown cipher name '{0}'".format(algorithm)
+            raise ValueError(msg)
+        ctx = api.new('EVP_CIPHER_CTX*')
+        api.EVP_CIPHER_CTX_init(ctx)
+        cleanup = lambda _: api.EVP_CIPHER_CTX_cleanup(ctx)
+        self._weakref = weakref.ref(self, cleanup)
+        self._ctx = ctx
+        api.EVP_CipherInit_ex(self._ctx, self._cipher,
+                api.NULL, api.NULL, api.NULL, 1 if encrypt else 0)
+
+    @property
+    def block_size(self):
+        if self._cipher == api.NULL:
+            raise ValueError("Cipher object failed to be initialised")
+        return api.EVP_CIPHER_block_size(self._cipher)
+
+    @property
+    def ivector_len(self):
+        if self._cipher == api.NULL:
+            raise ValueError("Cipher object failed to be initialised")
+        return api.EVP_CIPHER_iv_length(self._cipher)
+
+    @property
+    def key_len(self):
+        if self._ctx == api.NULL:
+            raise ValueError("Cipher object failed to be initialised")
+        return api.EVP_CIPHER_CTX_key_length(self._ctx)
+
+    @key_len.setter
+    def key_len(self, length):
+        if self._ctx == api.NULL:
+            raise ValueError("Cipher object failed to be initialised")
+        if not api.EVP_CIPHER_CTX_set_key_length(self._ctx, length):
+            messages = err.log_errors(level=None)
+            raise ValueError(messages[0])
+
+    @property
+    def decrypting(self):
+        return not self._encrypting
+
+    @property
+    def encrypting(self):
+        return self._encrypting
+
+    @property
+    def mode(self):
+        if self._cipher == api.NULL:
+            raise ValueError("Cipher object failed to be initialised")
+        return api.EVP_CIPHER_mode(self._cipher)
+
+    @property
+    def name(self):
+        if self._cipher == api.NULL:
+            raise ValueError("Cipher object failed to be initialised")
+        return bytes(api.OBJ_nid2sn(api.EVP_CIPHER_nid(self._cipher)))
