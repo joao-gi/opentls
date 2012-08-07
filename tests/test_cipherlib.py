25cc325a29cad4205fab620cd0a5d7e4ad1ba77a
Aaron Iles <aaron.iles@gmail.com>
2012-08-07 21:39:36 +1000

    Refactor unit tests for Cipher object
    Pulls common code for all cipher types into mixin class that are
    inherited by test classes.

--- tests/test_cipherlib.py
+++ tests/test_cipherlib.py
@@ -16,12 +16,7 @@ class TestAlgorithms(unittest.TestCase):
         self.assertIn(b'AES-128-CBC', cipherlib.algorithms_available)
 
 
-class TestAesEncryptObject(unittest.TestCase):
-
-    ALGORITHM = 'AES-128-CBC'
-    ENCRYPT = True
-    IVECTOR = '\x00' * 16
-    KEY = '\x00' * 16
+class CipherObject(object):
 
     def setUp(self):
         self.cipher = cipherlib.Cipher(self.ENCRYPT, self.ALGORITHM)
@@ -31,87 +26,65 @@ class TestAesEncryptObject(unittest.TestCase):
             del self.cipher
 
     def test_block_size(self):
-        self.assertEqual(16, self.cipher.block_size)
+        self.assertEqual(self.LEN_BLOCK, self.cipher.block_size)
 
     def test_ivector_len(self):
-        self.assertEqual(16, self.cipher.ivector_len)
+        self.assertEqual(self.LEN_IV, self.cipher.ivector_len)
 
     def test_key_len(self):
-        self.assertEqual(16, self.cipher.key_len)
+        self.assertEqual(self.LEN_KEY, self.cipher.key_len)
+
+    def test_decrypting(self):
+        self.assertNotEqual(self.ENCRYPT, self.cipher.decrypting)
+
+    def test_encrypting(self):
+        self.assertEqual(self.ENCRYPT, self.cipher.encrypting)
+
+    def test_mode(self):
+        self.assertEqual(self.MODE, self.cipher.mode)
+
+    def test_name(self):
+        self.assertEqual(self.ALGORITHM, self.cipher.name)
+
+    def test_invalid_name(self):
+        self.assertRaises(ValueError, cipherlib.Cipher, self.ENCRYPT, 'UNDEF')
+
+    def test_weakref_bio(self):
+        BIO_free_all_cleanup = api.BIO_free_all
+        with mock.patch('tls.c.api.BIO_free_all') as cleanup_mock:
+            cleanup_mock.side_effect = BIO_free_all_cleanup
+            del self.cipher
+            self.assertEqual(cleanup_mock.call_count, 1)
+
+
+class TestAesEncryptObject(CipherObject, unittest.TestCase):
+
+    ALGORITHM = 'AES-128-CBC'
+    ENCRYPT = True
+    IVECTOR = '\x00' * 16
+    KEY = '\x00' * 16
+    LEN_BLOCK = 16
+    LEN_IV = 16
+    LEN_KEY = 16
+    MODE = cipherlib.EVP_CIPH_CBC_MODE
 
     def test_key_len_set(self):
         def change_key_len(length):
             self.cipher.key_len = length
         self.assertRaises(ValueError, change_key_len, 8)
 
-    def test_decrypting(self):
-        self.assertFalse(self.cipher.decrypting)
 
-    def test_encrypting(self):
-        self.assertTrue(self.cipher.encrypting)
-
-    def test_mode(self):
-        self.assertEqual(cipherlib.EVP_CIPH_CBC_MODE, self.cipher.mode)
-
-    def test_name(self):
-        self.assertEqual('AES-128-CBC', self.cipher.name)
-
-    def test_invalid_name(self):
-        self.assertRaises(ValueError, cipherlib.Cipher, self.ENCRYPT, 'UNDEF')
-
-    def test_weakref_bio(self):
-        BIO_free_all_cleanup = api.BIO_free_all
-        with mock.patch('tls.c.api.BIO_free_all') as cleanup_mock:
-            cleanup_mock.side_effect = BIO_free_all_cleanup
-            del self.cipher
-            self.assertEqual(cleanup_mock.call_count, 1)
-
-
-class TestRc4DecryptObject(unittest.TestCase):
+class TestRc4DecryptObject(CipherObject, unittest.TestCase):
 
     ALGORITHM = 'RC4'
     ENCRYPT = False
     IVECTOR = ''
     KEY = '\x00' * 16
-
-    def setUp(self):
-        self.cipher = cipherlib.Cipher(self.ENCRYPT, self.ALGORITHM)
-
-    def tearDown(self):
-        if hasattr(self, 'cipher'):
-            del self.cipher
-
-    def test_block_size(self):
-        self.assertEqual(1, self.cipher.block_size)
-
-    def test_ivector_len(self):
-        self.assertEqual(0, self.cipher.ivector_len)
-
-    def test_key_len(self):
-        self.assertEqual(16, self.cipher.key_len)
+    LEN_BLOCK = 1
+    LEN_IV = 0
+    LEN_KEY = 16
+    MODE = cipherlib.EVP_CIPH_STREAM_CIPHER
 
     def test_key_len_set(self):
         self.cipher_key_len = 8
         # self.assertEqual(8, self.cipher.key_len)
-
-    def test_decrypting(self):
-        self.assertTrue(self.cipher.decrypting)
-
-    def test_encrypting(self):
-        self.assertFalse(self.cipher.encrypting)
-
-    def test_mode(self):
-        self.assertEqual(cipherlib.EVP_CIPH_STREAM_CIPHER, self.cipher.mode)
-
-    def test_name(self):
-        self.assertEqual('RC4', self.cipher.name)
-
-    def test_invalid_name(self):
-        self.assertRaises(ValueError, cipherlib.Cipher, self.ENCRYPT, 'UNDEF')
-
-    def test_weakref_bio(self):
-        BIO_free_all_cleanup = api.BIO_free_all
-        with mock.patch('tls.c.api.BIO_free_all') as cleanup_mock:
-            cleanup_mock.side_effect = BIO_free_all_cleanup
-            del self.cipher
-            self.assertEqual(cleanup_mock.call_count, 1)
