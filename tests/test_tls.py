1c36b6902cfde9c8cdd25313c72cd11e9e9073c2
Aaron Iles <aaron.iles@gmail.com>
2012-10-02 19:50:48 +1000

    Test for valid package version number.
    Validate version number using regular expression from PEP 386.
    http://www.python.org/dev/peps/pep-0386/

--- tests/test_tls.py
+++ tests/test_tls.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division, print_function
+import doctest
 
 try:
     import unittest
@@ -7,8 +8,16 @@ except ImportError:
 
 import tls
 
+PEP386 = r"""^(?P<version>\d+\.\d+)(?P<extraversion>(?:\.\d+)*)(?:(?P<prerel>[abc]|rc)(?P<prerelversion>\d+(?:\.\d+)*))?(?P<postdev>(\.post(?P<post>\d+))?(\.dev(?P<dev>\d+))?)?$"""
+
 
 class TestTls(unittest.TestCase):
 
-    def test_has_version(self):
-        self.assertTrue(tls.__version__)
+    def test_valid_version(self):
+        if hasattr(self, 'assertRegex'):
+            self.assertRegex(tls.__version__, PEP386)
+        else:
+            self.assertRegexpMatches(tls.__version__, PEP386)
+
+    def test_readme(self):
+        doctest.testfile('../README.rst')
