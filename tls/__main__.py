ec2ca459977fb43ac227d2176d3798fce7eb1603
Aaron Iles <aaron.iles@gmail.com>
2012-10-02 22:55:47 +1000

    Move executable module to tls.version
    Python2.6 does not support executable packages. The __main__.py module
    is moved to tls.version to print the OpenSSL library version is run.

--- tls/__main__.py
+++
@@ -1,7 +0,0 @@
-"""OpenTLS command line application"""
-from tls.c import api
-
-if __name__ == '__main__':
-    version = api.version()
-    template = 'OpenTLS {major}.{minor}.{fix}{patch} ({status})'
-    print(template.format(**version._asdict()))
--- tls/version.py
+++ tls/version.py
@@ -1,1 +1,8 @@
 __version__ = "0.1"
+
+from tls.c import api
+
+if __name__ == '__main__':
+    version = api.version()
+    template = 'OpenTLS {major}.{minor}.{fix}{patch} ({status})'
+    print(template.format(**version._asdict()))
